3bc5d287596ab0404883e608435446b6f8524467
Tonoward <tono_war@hotmail.com>
2021-10-14 20:03:15 +0900

    lelz

--- src/convert_image_to_costmap.py
+++ src/convert_image_to_costmap.py
@@ -4,24 +4,31 @@ import roslib
 import numpy
 import rospy
 import sys
-from std_msgs.msg import String,Int32,Int32MultiArray,MultiArrayLayout,MultiArrayDimension
+from std_msgs.msg import *
+from nav_msgs.msg import *
 from rospy.numpy_msg import numpy_msg
 from rospy_tutorials.msg import Floats
 import cv2
 import numpy as np
 import time
 
+width=500
+height=500
 
-imagetosend=[]
-data_to_send=Int32MultiArray()
+
+imagetosend=[0] * (width*height)
+
+data_to_send=Int8MultiArray()
+map_to_send=OccupancyGrid()
 
 ################################################################################################
 
 
 def main(args):
-   pub=rospy.Publisher('avoid_image',Int32MultiArray,queue_size=2)
+   pub=rospy.Publisher('avoid_image',Int8MultiArray,queue_size=1)
+   pub_map=rospy.Publisher('avoid_map',OccupancyGrid,queue_size=1)
    rospy.init_node('avoid_image')
-   r = rospy.Rate(2)
+   r = rospy.Rate(100)
    while not rospy.is_shutdown():
         cap = cv2.VideoCapture(0)
         cap.isOpened()
@@ -34,7 +41,7 @@ def main(args):
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 imgCanny = cv2.Canny(frame, 150, 200)  #Detecta bordes
 
-                imgResize = cv2.resize(imgCanny,(500, 500)) #Cambia el ancho y alto de la imagen
+                imgResize = cv2.resize(imgCanny,(width, height)) #Cambia el ancho y alto de la imagen
 
                 list=imgResize.tolist()
 
@@ -42,14 +49,37 @@ def main(args):
 
                 #print("\nHello\n")
                 #print(len(list))
+
+                k=0
+
                 for i in range(len(list)):
                     for j in range(len(list)):
                         if list[i][j]>0:
                             list[i][j]=100
-                        imagetosend.append(list[i][j])
+                        imagetosend[k]=list[i][j] 
+                        k+=1
                 #print(list)
                 #print(imagetosend)
+                
+
                 data_to_send.data=imagetosend
+
+                #Datos del mapa a mandar
+                map_to_send.header.frame_id = "base_link"
+                map_to_send.info.width = width
+                map_to_send.info.height = height
+                map_to_send.info.resolution = 0.01
+                map_to_send.info.origin.position.x = 1
+                map_to_send.info.origin.position.y = -0.5
+                map_to_send.info.origin.position.z = 0
+                map_to_send.info.origin.orientation.z = 1
+                map_to_send.info.origin.orientation.w = 1
+
+
+
+                map_to_send.data=imagetosend
+                pub_map.publish(map_to_send)
+
                 pub.publish(data_to_send)
                 r.sleep()   
                 # Display the resulting frame
